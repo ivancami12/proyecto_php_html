6a133162ae05c032e89d6b4bbe93e60b1103177d
Ivan <hernandezivan12zx@gmail.com>
2024-11-07 09:40:31 -0500

    fix: configuracion en Json para el estilo y formateo

--- estilos/js/scripts.js
+++ estilos/js/scripts.js
@@ -8,29 +8,28 @@ adminSection.style.display = "none";
 
 // Evento para alternar la visibilidad de la sección de administrador
 btnActualizar.addEventListener("click", function () {
-    if (isAdmin) {
-        if (adminSection.style.display === "none") {
-            adminSection.style.display = "block";
-        } else {
-            adminSection.style.display = "none";
-        }
+  if (isAdmin) {
+    if (adminSection.style.display === "none") {
+      adminSection.style.display = "block";
     } else {
-        alert("No tienes permisos para ver esta sección.");
+      adminSection.style.display = "none";
     }
+  } else {
+    alert("No tienes permisos para ver esta sección.");
+  }
 });
 
 // Función para la animación de la lista de tareas
-window.onload = function() {
-    var listaTareas = document.getElementById('listaTareas');
+window.onload = function () {
+  var listaTareas = document.getElementById("listaTareas");
 
-    // Añadir la animación
-    listaTareas.classList.add('slide-animation');
-    
-    // Después de 3 segundos, quitar la transición
-    setTimeout(function() {
-        listaTareas.style.transition = 'none';
-        listaTareas.style.transform = 'translateY(0)';
-        listaTareas.style.opacity = '1';
-    }, 3000); // 3 segundos de espera
+  // Añadir la animación
+  listaTareas.classList.add("slide-animation");
+
+  // Después de 3 segundos, quitar la transición
+  setTimeout(function () {
+    listaTareas.style.transition = "none";
+    listaTareas.style.transform = "translateY(0)";
+    listaTareas.style.opacity = "1";
+  }, 3000); // 3 segundos de espera
 };
-    
